f33c0949bc280d34c632a11e0dc7bd44a1f2f951
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-08-10 21:22:37 -0300

    add lombok getters e setters

--- src/main/java/com/example/crud/models/ProdutoEntity.java
+++ src/main/java/com/example/crud/models/ProdutoEntity.java
@@ -1,38 +1,22 @@
 package com.example.crud.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class ProdutoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private long id;
     @Column(name = "nome")
+    @Getter
+    @Setter
     private String nome;
     @Column(name = "quantidade")
+    @Getter
+    @Setter
     private int quantidade;
-
-    public long getId(){
-        return this.id;
-    }
-
-    public void setId(long id){
-        this.id = id;
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public int getQuantidade(){
-        return this.quantidade;
-    }
-
-    public void setQuantidade(int quantidade){
-        this.quantidade = quantidade;
-    }
 }
